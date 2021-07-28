c11ad90cba0f542dbe8bba3752eeb1c9ab88175f
emsesc <emseschen@gmail.com>
2021-07-27 23:08:30 -0400

    html edits

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ var path = __dirname;
 app.use('/', router);
 app.use('/assets', express.static(path + '/assets'))
 
-// this is telling the website WHERE all of our "asset" files are. Asset files include CSS for styling, JS for Bootstrap to make it pretty, and images.
-  
+// challenge submission
+
+router.get('/challenge',function(req,res){
+  res.sendFile(path + '/pages/challenge.html');
+});
+
+// home and terminal page
 router.get('/home',function(req, res){
   res.sendFile(path + '/pages/index.html');
 });
@@ -27,34 +32,32 @@ router.get('/',function(req, res){
   res.sendFile(path + '/pages/terminal.html');
 });
 
-// The logic here: when someone navigates to www.cybercatamounts.herokuapp.com, the website will return the content on the "index.html" page.
-// Why is it router.get? Because when you enter something in your browser, you are making a GET request.
-
 router.get('/about',function(req, res){
     res.sendFile(path + '/pages/about.html');
   });
 
+
+// other pages
 router.get('/services',function(req,res){
   res.sendFile(path + '/pages/services.html');
 });
-router.get('/portfolio',function(req,res){
-  res.sendFile(path + '/pages/portfolio.html');
-});
+
 router.get('/index',function(req,res){
   res.sendFile(path + '/pages/index.html');
 });
+
 router.get('/blog',function(req,res){
   res.sendFile(path + '/pages/blog.html');
 });
+
 router.get('/team',function(req,res){
   res.sendFile(path + '/pages/team.html');
 });
-router.get('/challenge',function(req,res){
-  res.sendFile(path + '/pages/contact.html');
-});
+
 router.get('/blog/test-blog',function(req,res){
   res.sendFile(path + '/pages/blog-single.html');
 });
+
 // Try navigating to the /about directory on the website. Now, the about.html page will show.
 // Note: it won't work if you click the "about" button because we haven't changed the code there!
 
